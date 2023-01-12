ad7912c24f94c91e6ff2fbc141180174b378fe07
LonelyFirefly <Itisperfectworld@gmail.com>
2023-01-12 17:59:52 +0400

    change whole header section and mobile slide menu

--- mobile-slide-menu.js
+++ mobile-slide-menu.js
@@ -1,22 +1,26 @@
 "use strict";
 
-let menu = document.querySelector(".navbar__menu");
-let hamburger = document.querySelector(".navbar__hamburger");
-let logo = document.querySelector(".navbar__logo");
-let header = document.querySelector("header");
+let menu = document.querySelector(".header__menu");
+let hamburger = document.querySelector(".header__hamburger");
+let logo = document.querySelector(".header__logo");
 
-console.log(window.scrollTop);
-console.log(window.pageXOffset);
-
-function changeHamburgerIcon(hamburger) {
-	hamburger.innerHTML =
-		'<svg xmlns="http://www.w3.org/2000/svg" width="16" height="15"><path fill="#000000" fill-rule="evenodd" d="M8 5.379L13.303.075l2.122 2.122L10.12 7.5l5.304 5.303-2.122 2.122L8 9.62l-5.303 5.304-2.122-2.122L5.88 7.5.575 2.197 2.697.075 8 5.38z"/></svg>';
-
+hamburger.addEventListener("click", () => {
 	hamburger.classList.toggle("active");
-	if (!hamburger.classList.contains("active")) {
-		hamburger.innerHTML =
-			'<svg xmlns="http://www.w3.org/2000/svg" width="18" height="15"><path fill="#242A45" fill-rule="evenodd" d="M0 0h18v3H0V0zm0 6h18v3H0V6zm0 6h18v3H0v-3z"/></svg>';
-	}
+	menu.classList.toggle("active");
+	document.body.classList.toggle("lock");
+	changeLogoIcon(logo);
+});
+
+function changeLogoIconToDark(logoName, circle, circleImage) {
+	logoName.setAttribute("fill", "#242A45");
+	circle.setAttribute("fill", "#5267DF");
+	circleImage.setAttribute("fill", "#FFF");
+}
+
+function changeLogoIconToLight(logoName, circle, circleImage) {
+	logoName.setAttribute("fill", "#FFF");
+	circle.setAttribute("fill", "#FFF");
+	circleImage.setAttribute("fill", "#242A45");
 }
 
 function changeLogoIcon(logo) {
@@ -24,60 +28,28 @@ function changeLogoIcon(logo) {
 	let circle = logo.children[0].children[0].children[1].children[0];
 	let circleImage = logo.children[0].children[0].children[1].children[1];
 
-	if (logo.classList.contains("active")) {
-		logoName.setAttribute("fill", "#242A45");
-		circle.setAttribute("fill", "#5267DF");
-		circleImage.setAttribute("fill", "#FFF");
+	if (!hamburger.classList.contains("active")) {
+		changeLogoIconToDark(logoName, circle, circleImage);
 	} else {
-		logoName.setAttribute("fill", "#FFF");
-		circle.setAttribute("fill", "#FFF");
-		circleImage.setAttribute("fill", "#242A45");
+		changeLogoIconToLight(logoName, circle, circleImage);
 	}
+	window.addEventListener("resize", function () {
+		if (window.matchMedia("(max-width: 567px)").matches) {
+			changeLogoIconToLight(logoName, circle, circleImage);
+		} else {
+			changeLogoIconToDark(logoName, circle, circleImage);
+		}
+	});
+
 	logo.classList.toggle("active");
 }
 
-function changePaddingMargin() {
-	let navbarHeader = document.querySelector(".navbar__header");
-	let activeMenu = document.querySelector(".navbar__menu.active");
-	if (menu.classList.contains("active")) {
-		header.style.padding = "0";
-		header.style.height = "auto";
-		navbarHeader.style.paddingLeft = "11%";
-		navbarHeader.style.paddingRight = "6%";
-		navbarHeader.style.marginTop = "45px";
-		document.body.style.marginTop = "0px";
-	} else {
-		header.style.paddingLeft = "11%";
-		header.style.paddingRight = "6%";
-		header.style.paddingBottom = "80px";
-
-		header.style.height = "46px";
-		navbarHeader.style.paddingLeft = "";
-		navbarHeader.style.paddingRight = "";
-		navbarHeader.style.marginTop = "0";
-		document.body.style.marginTop = "45px";
-	}
-}
-
-function changeBodyScroll() {
-	if (menu.classList.contains("active")) {
-		document.body.style.overflow = "hidden";
-	} else {
-		document.body.style.overflow = "";
-	}
-}
-
-function changeBodyLayout() {
-	changePaddingMargin();
-	changeBodyScroll();
-}
-
-hamburger.addEventListener("click", () => {
-	menu.classList.toggle("active");
-	changeHamburgerIcon(hamburger);
-	changeLogoIcon(logo);
-	changeBodyLayout();
-});
+// hamburger.addEventListener("click", () => {
+// 	menu.classList.toggle("active");
+// 	changeHamburgerIcon(hamburger);
+// 	changeLogoIcon(logo);
+// 	changeBodyLayout();
+// });
 
 document.querySelectorAll(".navbar__link").forEach((item) =>
 	item.addEventListener("click", () => {
