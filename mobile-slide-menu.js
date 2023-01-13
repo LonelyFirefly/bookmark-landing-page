136dfbb20c93c1573e10d9e4f8afaa429be83368
LonelyFirefly <Itisperfectworld@gmail.com>
2023-01-13 13:30:35 +0400

    fix menu

--- mobile-slide-menu.js
+++ mobile-slide-menu.js
@@ -4,12 +4,58 @@ let menu = document.querySelector(".header__menu");
 let hamburger = document.querySelector(".header__hamburger");
 let logo = document.querySelector(".header__logo");
 
-hamburger.addEventListener("click", () => {
+window.addEventListener("resize", () => {
+	if (document.documentElement.clientWidth < 657) {
+		menu.addEventListener("click", removeMenu);
+	} else {
+		menu.removeEventListener("click", removeMenu);
+	}
+});
+
+hamburger.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
 	hamburger.classList.toggle("active");
 	menu.classList.toggle("active");
 	document.body.classList.toggle("lock");
 	changeLogoIcon(logo);
-});
+}
+
+function removeMenu(e) {
+	let targetItem = e.target;
+	console.log(targetItem);
+	if (targetItem.closest(".header__link")) {
+		toggleMenu();
+	}
+}
+
+function changeLogoIcon(logo) {
+	let logoName = logo.children[0].children[0].children[0];
+	let circle = logo.children[0].children[0].children[1].children[0];
+	let circleImage = logo.children[0].children[0].children[1].children[1];
+
+	if (
+		!hamburger.classList.contains("active") ||
+		logo.classList.contains("active")
+	) {
+		changeLogoIconToDark(logoName, circle, circleImage);
+	} else {
+		changeLogoIconToLight(logoName, circle, circleImage);
+	}
+}
+
+function changeLogoIconDependingOnScreenSize() {
+	if (window.matchMedia("(max-width: 567px)").matches) {
+		logo.classList.add("active");
+		changeLogoIcon(logo);
+		logo.classList.remove("active");
+		console.log("<767");
+	} else {
+		logo.classList.remove("active");
+		changeLogoIcon(logo);
+		console.log(">767");
+	}
+}
 
 function changeLogoIconToDark(logoName, circle, circleImage) {
 	logoName.setAttribute("fill", "#242A45");
@@ -23,38 +69,22 @@ function changeLogoIconToLight(logoName, circle, circleImage) {
 	circleImage.setAttribute("fill", "#242A45");
 }
 
-function changeLogoIcon(logo) {
-	let logoName = logo.children[0].children[0].children[0];
-	let circle = logo.children[0].children[0].children[1].children[0];
-	let circleImage = logo.children[0].children[0].children[1].children[1];
+// window.addEventListener("resize", changeLogoIconDependingOnScreenSize);
 
-	if (!hamburger.classList.contains("active")) {
-		changeLogoIconToDark(logoName, circle, circleImage);
-	} else {
-		changeLogoIconToLight(logoName, circle, circleImage);
-	}
-	window.addEventListener("resize", function () {
-		if (window.matchMedia("(max-width: 567px)").matches) {
-			changeLogoIconToLight(logoName, circle, circleImage);
-		} else {
-			changeLogoIconToDark(logoName, circle, circleImage);
-		}
-	});
-
-	logo.classList.toggle("active");
-}
-
-// hamburger.addEventListener("click", () => {
-// 	menu.classList.toggle("active");
-// 	changeHamburgerIcon(hamburger);
-// 	changeLogoIcon(logo);
-// 	changeBodyLayout();
-// });
-
-document.querySelectorAll(".navbar__link").forEach((item) =>
-	item.addEventListener("click", () => {
-		menu.classList.remove("active");
-		closeIcon.classList.remove("active");
-		hamburger.classList.remove("active");
-	})
-);
+// document.querySelectorAll(".navbar__link").forEach((item) =>
+// 	item.addEventListener("click", () => {
+// 		menu.classList.remove("active");
+// 		closeIcon.classList.remove("active");
+// 		hamburger.classList.remove("active");
+// 	})
+// );
+// if (window.matchMedia("(max-width: 567px)").matches) {
+// 	console.log("true");
+// 	menu.addEventListener("click", (e) => {
+// 		let targetItem = e.target;
+// 		console.log(targetItem);
+// 		if (targetItem.closest(".header__link")) {
+// 			toggleMenu();
+// 		}
+// 	});
+// }
